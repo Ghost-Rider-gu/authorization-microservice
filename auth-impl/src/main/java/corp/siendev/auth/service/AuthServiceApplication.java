867dbdad14e90930a7fdfe1ea8043887af440aea
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-08-06 22:37:37 +0300

    update app.properties

--- auth-impl/src/main/java/corp/siendev/auth/service/AuthServiceApplication.java
+++ auth-impl/src/main/java/corp/siendev/auth/service/AuthServiceApplication.java
@@ -3,11 +3,18 @@ package corp.siendev.auth.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Auth service application invoke.
+ *
+ * @author Golubnichenko_Yuriy
+ */
 @SpringBootApplication
 public class AuthServiceApplication {
 
     public static void main(String args[]) {
-        SpringApplication.run(AuthServiceApplication.class, args);
+        SpringApplication application = new SpringApplication(AuthServiceApplication.class);
+
+        application.run(args);
     }
 
 }
