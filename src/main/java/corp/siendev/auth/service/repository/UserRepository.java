b7c65d087ad1af8361748af0fb60fd9f492b2028
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-08-27 20:02:19 +0300

    update UserRepository

--- src/main/java/corp/siendev/auth/service/repository/UserRepository.java
+++ src/main/java/corp/siendev/auth/service/repository/UserRepository.java
@@ -1,23 +1,13 @@
 package corp.siendev.auth.service.repository;
 
 import corp.siendev.auth.service.entity.User;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * User repository interface.
  *
  * @author Golubnichenko_Yuriy
  */
-public interface UserRepository {
-
-    void create(User user);
-    void update(User user);
-    void delete(User user);
-    void save();
-
-    User getUser(int userId);
-    List<User> getAllUsers();
-    User findById(int userId);
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
