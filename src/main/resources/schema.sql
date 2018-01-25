92bb6e280b111b0a81332fe9d1eeeaf270c4c1c5
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2018-01-25 22:26:14 +0300

    update data base schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,8 +5,8 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema auth_service
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `auth_service` DEFAULT CHARACTER SET utf8 ;
-USE `auth_service` ;
+CREATE SCHEMA IF NOT EXISTS `auth_service` DEFAULT CHARACTER SET utf8;
+USE `auth_service`;
 
 -- -----------------------------------------------------
 -- Table `auth_service`.`user`
@@ -41,35 +41,6 @@ CREATE TABLE IF NOT EXISTS `auth_service`.`role` (
   `role_name` VARCHAR(20) NOT NULL,
   `role_description` VARCHAR(50) NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
--- -----------------------------------------------------
--- Table `auth_service`.`user_assigment`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `auth_service`.`user_assigment` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` BIGINT(9) NOT NULL,
-  `role_id` INT NOT NULL,
-  `project_module_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `user_id_idx` (`user_id` ASC),
-  INDEX `role_id_idx` (`role_id` ASC),
-  INDEX `project_module_id_idx` (`project_module_id` ASC),
-  CONSTRAINT `user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `auth_service`.`user` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `role_id`
-    FOREIGN KEY (`role_id`)
-    REFERENCES `auth_service`.`role` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT `project_module_id`
-    FOREIGN KEY (`project_module_id`)
-    REFERENCES `auth_service`.`project_module` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 SET SQL_MODE=@OLD_SQL_MODE;
