22f334c385abc91ff0bd5471bdf4e1c43a366190
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2018-01-28 23:28:26 +0300

    add context loads test

---
+++ src/test/java/corp/siendev/auth/service/AuthServiceApplicationTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2018 SienDev company. All rights reserved.
+ *
+ * This is a free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ */
+
+package corp.siendev.auth.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthServiceApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
