5d2c1eb6a50792c7ac0a0f4cc51741833f1f4f1a
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2018-02-19 23:27:51 +0300

    access handler + admin panel controller

---
+++ src/main/java/corp/siendev/auth/service/config/AuthServiceAccessHandler.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2018 SienDev company. All rights reserved.
+ *
+ * This is a free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ */
+
+package corp.siendev.auth.service.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class AuthServiceAccessHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest,
+                       HttpServletResponse httpServletResponse,
+                       AccessDeniedException e) throws IOException, ServletException {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null) {
+            log.info("");
+        }
+
+        httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/403");
+    }
+}
