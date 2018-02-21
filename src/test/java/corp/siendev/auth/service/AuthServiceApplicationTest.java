1708b5993ce5652c10cda17db725150af13548cc
Golubnichenko Yuriy <ghostrider.gu@gmail.com>
2018-02-21 14:04:38 +0300

    Update AuthServiceApplicationTest.java

--- src/test/java/corp/siendev/auth/service/AuthServiceApplicationTest.java
+++ src/test/java/corp/siendev/auth/service/AuthServiceApplicationTest.java
@@ -12,6 +12,7 @@
 package corp.siendev.auth.service;
 
 import org.junit.Test;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
